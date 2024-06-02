a37f84c9552b0e0bb25ef4aff9494e061f1bf62c
CherkashinDD <danl-0284@mail.ru>
2024-06-02 21:07:24 +1000

    Add files via upload

---
+++ mani.py
@@ -0,0 +1,30 @@
+#Длина слова
+a="длина"
+print(len(a))
+
+#Суммы и разности
+first=546
+second=342
+summa=first+second
+diff=first-second
+print(summa)
+print(diff)
+
+#Среднее арифметическое
+first=546
+second=342
+third=8
+mean=(first+second+third)/3
+print(mean)
+
+#Простые строчки
+first_string="Вторник"
+second_string="Понедельник"
+print(second_string+', '+first_string)
+
+#Сложная формула
+a=25
+b=28
+c=34
+f=(((a*b)+(a*c))**3)/2
+print(f)
